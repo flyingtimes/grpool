042aef94e10c6fa7ed9a65c8f40128dae277b44a
flyingtimes <5200374@qq.com>
2017-12-30 21:37:45 +0800

    Add files via upload
    ok

--- grpool_test.go
+++ grpool_test.go
@@ -1,103 +1,28 @@
-package grpool
+package main
 
 import (
-	"io/ioutil"
-	"log"
+	"fmt"
+	"github.com/flyingtimes/grpool"
 	"runtime"
-	"sync/atomic"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
+	"time"
 )
 
-func init() {
-	println("using MAXPROC")
-	numCPUs := runtime.NumCPU()
-	runtime.GOMAXPROCS(numCPUs)
+func printit(ino []interface{}) interface{} {
+	i := ino[0].(int)
+	var j = (i*i + 2*i + i) * i
+	return fmt.Sprintf("from callback %d", j)
 }
-
-func TestNewWorker(t *testing.T) {
-	pool := make(chan *worker)
-	worker := newWorker(pool)
-	worker.start()
-	assert.NotNil(t, worker)
-
-	worker = <-pool
-	assert.NotNil(t, worker, "Worker should register itself to the pool")
-
-	called := false
-	done := make(chan bool)
-
-	job := func() {
-		called = true
-		done <- true
-	}
-
-	worker.jobChannel <- job
-	<-done
-	assert.Equal(t, true, called)
-}
-
 func TestNewPool(t *testing.T) {
-	pool := NewPool(1000, 10000)
+	start := time.Now()
+	runtime.GOMAXPROCS(4)
+	pool := grpool.NewPool(10, 30)
 	defer pool.Release()
-
-	iterations := 1000000
-	pool.WaitCount(iterations)
-	var counter uint64 = 0
-
-	for i := 0; i < iterations; i++ {
-		arg := uint64(1)
-
-		job := func() {
-			defer pool.JobDone()
-			atomic.AddUint64(&counter, arg)
-			assert.Equal(t, uint64(1), arg)
-		}
-
-		pool.JobQueue <- job
+	for i := 0; i < 10000; i++ {
+		var jb grpool.Job
+		jb.SetCallback(printit, i)
+		pool.JobQueue <- jb
 	}
-
-	pool.WaitAll()
-
-	counterFinal := atomic.LoadUint64(&counter)
-	assert.Equal(t, uint64(iterations), counterFinal)
-}
-
-func TestRelease(t *testing.T) {
-	grNum := runtime.NumGoroutine()
-	pool := NewPool(5, 10)
-	defer func() {
-		pool.Release()
-
-		// give some time for all goroutines to quit
-		assert.Equal(t, grNum, runtime.NumGoroutine(), "All goroutines should be released after Release() call")
-	}()
-
-	pool.WaitCount(1000)
-
-	for i := 0; i < 1000; i++ {
-		job := func() {
-			defer pool.JobDone()
-		}
-
-		pool.JobQueue <- job
-	}
-
-	pool.WaitAll()
-}
-
-func BenchmarkPool(b *testing.B) {
-	// Testing with just 1 goroutine
-	// to benchmark the non-parallel part of the code
-	pool := NewPool(1, 10)
-	defer pool.Release()
-
-	log.SetOutput(ioutil.Discard)
-
-	for n := 0; n < b.N; n++ {
-		pool.JobQueue <- func() {
-			log.Printf("I am worker! Number %d\n", n)
-		}
-	}
+	elapsed := time.Since(start)
+	fmt.Println(elapsed)
 }
